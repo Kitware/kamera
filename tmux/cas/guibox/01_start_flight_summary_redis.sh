7ef90720c6909e7b6b89244ae9671ba982b2f4f5
Adam Romlein <adam.romlein@gmail.com>
2024-11-12 17:11:08 +0000

    Move home dir to kamera from noaa_kamera

--- tmux/cas/guibox/01_start_flight_summary_redis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $DIR/../env.sh
-
-python /home/user/kw/postflight_scripts/scripts/create_flight_summary_redis.py
