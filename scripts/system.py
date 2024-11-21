0247f9c1d8aed1a939d74ed9a276dd4b1c9601cf
Adam Romlein <adam.romlein@gmail.com>
2024-11-21 01:10:38 +0000

    Add cam_uv to taiga, remove from cas

--- scripts/system.py
+++ scripts/system.py
@@ -1,4 +1,5 @@
 import sys
+
 # import xmlrpclib # Python2
 from xmlrpc.client import ServerProxy, Fault
 
@@ -8,11 +9,24 @@ with open("/home/user/kw/SYSTEM_NAME") as f:
 if SYSTEM_NAME == "taiga":
     group = "taiga"
     hosts = ["nuvo0", "nuvo1", "nuvo2"]
-    pod = ["image_manager", "kamerad", f"{group}:imageview", f"{group}:cam_rgb", f"{group}:cam_ir"]
+    pod = [
+        "image_manager",
+        "kamerad",
+        f"{group}:imageview",
+        f"{group}:cam_rgb",
+        f"{group}:cam_ir",
+        f"{group}:cam_uv",
+    ]
 else:
     group = "cas"
     hosts = ["cas0", "cas1", "cas2"]
-    pod = ["image_manager", "kamerad", f"{group}:imageview", f"{group}:cam_uv", f"{group}:cam_rgb", f"{group}:cam_ir"]
+    pod = [
+        "image_manager",
+        "kamerad",
+        f"{group}:imageview",
+        f"{group}:cam_rgb",
+        f"{group}:cam_ir",
+    ]
 
 host = sys.argv[1].strip()
 if host not in hosts:
@@ -20,16 +34,17 @@ if host not in hosts:
     raise SystemExit
 action = sys.argv[2]
 cluster = sys.argv[3]
-group2processes= {"pod":pod,
-                  "central":[f"{group}:ins", f"{group}:daq"],
-                  "monitor":[f"{group}:cam_param_monitor", f"{group}:shapefile_monitor"],
-                  "master": ["roscore"],
-                  "nas": ["mount_nas"],
-                  "detector": [f"{group}:detector"]
-                 }
+group2processes = {
+    "pod": pod,
+    "central": [f"{group}:ins", f"{group}:daq"],
+    "monitor": [f"{group}:cam_param_monitor", f"{group}:shapefile_monitor"],
+    "master": ["roscore"],
+    "nas": ["mount_nas"],
+    "detector": [f"{group}:detector"],
+}
 processes = group2processes[cluster]
 # sup = xmlrpclib.Server('http://%s:9001/RPC2' % host)
-sup = ServerProxy('http://%s:9001/RPC2' % host)
+sup = ServerProxy("http://%s:9001/RPC2" % host)
 
 print("Executing action %s on host %s with cluster %s." % (action, host, cluster))
 for process in processes:
