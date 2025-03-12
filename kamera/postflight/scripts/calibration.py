6185fe58289064f5e977be96e3163f9a8196cf13
Adam Romlein <adam.romlein@gmail.com>
2025-03-12 12:35:07 -0400

    Add IR calibration and GIF generation

--- kamera/postflight/scripts/calibration.py
+++ kamera/postflight/scripts/calibration.py
@@ -6,27 +6,11 @@ from kamera.colmap_processing.camera_models import load_from_file
 from kamera.postflight.colmap import ColmapCalibration, find_best_sparse_model
 
 
-def main():
-    # REQUIREMENTS: Must already have built a reasonable 3-D model using Colmap
-    # And have a flight directory populated by the kamera system
-    flight_dir = "/home/local/KHQ/adam.romlein/noaa/data/2024_AOC_AK_Calibration/fl09"
-    colmap_dir = os.path.join(flight_dir, "colmap")
-    # Location to save KAMERA camera models.
-    save_dir = os.path.join(flight_dir, "kamera_models")
-    # Location to find / build aligned model
-    align_dir = os.path.join(colmap_dir, "aligned")
-    # This assumes that 2 modalities of cameras are within the 3-D model
-    joint_calibration = True
-    # Can switch to UV / IR if possible that they are better aligned
-    main_modality = "rgb"
-    # If camera models are already present, if true this will overwrite
-    force_calibrate = False
-    # Whether to calibrate IR models or not. A "colmap_ir" must be built
-    # in the same folder
-    calibrate_ir = True
-
-    # Step 1: Make sure the model is aligned to the INS readings, if not,
-    # find the best model and align it.
+def align_model(
+    flight_dir: os.PathLike | str,
+    colmap_dir: os.PathLike | str,
+    align_dir: os.PathLike | str,
+) -> ColmapCalibration:
     if os.path.exists(align_dir) and len(os.listdir(align_dir)) == 1:
         print(
             f"The directory {align_dir} exists and is not empty,"
@@ -53,6 +37,31 @@ def main():
         ub.ensuredir(aligned_sparse_dir)
         cc = ColmapCalibration(flight_dir, best_reconstruction_dir)
         cc.align_model(aligned_sparse_dir)
+    return cc
+
+
+def main():
+    # REQUIREMENTS: Must already have built a reasonable 3-D model using Colmap
+    # And have a flight directory populated by the kamera system
+    flight_dir = "/home/local/KHQ/adam.romlein/noaa/data/2024_AOC_AK_Calibration/fl09"
+    colmap_dir = os.path.join(flight_dir, "colmap")
+    # Location to save KAMERA camera models.
+    save_dir = os.path.join(flight_dir, "kamera_models")
+    # Location to find / build aligned model
+    align_dir = os.path.join(colmap_dir, "aligned")
+    # This assumes that 2 modalities of cameras are within the 3-D model
+    joint_calibration = True
+    # Can switch to UV / IR if possible that they are better aligned
+    main_modality = "rgb"
+    # If camera models are already present, if true this will overwrite
+    force_calibrate = False
+    # Whether to calibrate IR models or not. A "colmap_ir" must be built
+    # in the same folder
+    calibrate_ir = True
+
+    # Step 1: Make sure the model is aligned to the INS readings, if not,
+    # find the best model and align it.
+    cc = align_model(flight_dir, colmap_dir, align_dir)
 
     # We now have an aligned 3-D model, so we can calibrate the cameras
     print("Preparing calibration data...")
@@ -101,41 +110,98 @@ def main():
             # now calibrate all other modalities
             if modality == main_modality:
                 continue
-            print(f"Calibrating camera {camera_str}.")
-            tic = time.perf_counter()
-            camera_model = cc.transfer_calibration(
-                camera_str,
-                cameras[channel][main_modality],
-                calibrated_modality=main_modality,
-                error_threshold=100,
-            )
-            toc = time.perf_counter()
-            if camera_model is not None:
-                out_path = os.path.join(save_dir, f"{camera_str}_v2.yaml")
-                print(f"Saving camera model to {out_path}")
-                camera_model.save_to_file(out_path)
+            out_path = os.path.join(save_dir, f"{camera_str}_v2.yaml")
+            if os.path.exists(out_path) and not force_calibrate:
                 print(
-                    f"Time to transfer camera calibration to {camera_str} was {(toc - tic):.3f}s"
+                    f"Camera model path {out_path} exists and force_calibrate is turned off, loading model."
                 )
-                print(camera_model)
+                camera_model = load_from_file(out_path)
                 cameras[channel][modality] = camera_model
             else:
-                print(f"Transferring calibration to camera {camera_str} failed.")
+                print(f"Calibrating camera {camera_str}.")
+                tic = time.perf_counter()
+                camera_model = cc.transfer_calibration(
+                    camera_str,
+                    cameras[channel][main_modality],
+                    calibrated_modality=main_modality,
+                    error_threshold=100,
+                )
+                toc = time.perf_counter()
+                if camera_model is not None:
+                    print(f"Saving camera model to {out_path}")
+                    camera_model.save_to_file(out_path)
+                    print(
+                        f"Time to transfer camera calibration to {camera_str} was {(toc - tic):.3f}s"
+                    )
+                    print(camera_model)
+                    cameras[channel][modality] = camera_model
+                else:
+                    print(f"Transferring calibration to camera {camera_str} failed.")
 
     # IR calibration generally happens in a separate model, since SIFT has a hard
     # time matching between EO and long-wave IR
     if calibrate_ir:
-        colmap_dir = os.path.join(flight_dir, "colmap")
+        ir_colmap_dir = os.path.join(flight_dir, "colmap_ir")
+        ir_align_dir = os.path.join(ir_colmap_dir, "aligned")
+        ir_cc = align_model(flight_dir, ir_colmap_dir, ir_align_dir)
+        print("Preparing IR calibration data...")
+        ir_cc.prepare_calibration_data()
+        camera_strs = list(ir_cc.ccd.best_cameras.keys())
+        for camera_str in ir_cc.ccd.best_cameras.keys():
+            channel, modality = camera_str.split("_")[2:]
+            out_path = os.path.join(save_dir, f"{camera_str}_v2.yaml")
+            if os.path.exists(out_path) and not force_calibrate:
+                print(
+                    f"Camera model path {out_path} exists and force_calibrate is turned off, loading model."
+                )
+                camera_model = load_from_file(out_path)
+                cameras[channel][modality] = camera_model
+            else:
+                print(f"Calibrating camera {camera_str}.")
+                tic = time.perf_counter()
+                camera_model = ir_cc.calibrate_camera(camera_str, error_threshold=100)
+                toc = time.perf_counter()
+                if camera_model is not None:
+                    print(f"Saving camera model to {out_path}")
+                    camera_model.save_to_file(out_path)
+                    print(
+                        f"Time to calibrate camera {camera_str} was {(toc - tic):.3f}s"
+                    )
+                    print(camera_model)
+                    cameras[channel][modality] = camera_model
+                else:
+                    print(f"Calibrating camera {camera_str} failed.")
 
     aircraft, angle = camera_str.split("_")[0:2]
     gif_dir = os.path.join(save_dir, "registration_gifs_v2")
     ub.ensuredir(gif_dir)
+
+    # Write UV GIFs
     for channel in cameras.keys():
         rgb_model = cameras[channel]["rgb"]
         uv_model = cameras[channel]["uv"]
-        rgb_name = "_".join([aircraft, angle, channel, "rgb"])
         uv_name = "_".join([aircraft, angle, channel, "uv"])
-        cc.write_gifs(gif_dir, colmap_dir, rgb_name, uv_name, rgb_model, uv_model)
+        cc.write_gifs(
+            gif_dir,
+            camera_name=uv_name,
+            colocated_modality="rgb",
+            camera_model=uv_model,
+            colocated_camera_model=rgb_model,
+        )
+
+    # Write IR GIFs
+    if calibrate_ir:
+        for channel in cameras.keys():
+            rgb_model = cameras[channel]["rgb"]
+            ir_model = cameras[channel]["ir"]
+            ir_name = "_".join([aircraft, angle, channel, "ir"])
+            ir_cc.write_gifs(
+                gif_dir,
+                camera_name=ir_name,
+                colocated_modality="rgb",
+                camera_model=ir_model,
+                colocated_camera_model=rgb_model,
+            )
 
 
 if __name__ == "__main__":
