67423dc29d0780ef2c9262a110cd58c851ea5b83
romleiaj <adam.romlein@kitware.com>
2025-03-24 07:31:44 -0400

    Exclude the flight_data_mounted sentinel file from copy

--- src/run_scripts/entry/data_migration.sh
+++ src/run_scripts/entry/data_migration.sh
@@ -39,7 +39,7 @@ do
     echo "Gathering and copying text, detection, and log files older than 10 seconds under $source_dir"
     # Find all files that are appended to and copy over incrementally.
     # This is detection files, log files, and ins dat files
-    fdfind . $source_dir --exclude '*.{IIQ,iiq,json,tif,jpg}' --changed-before 10s --type f --exec echo {} | \
+    fdfind . $source_dir --exclude '*.{IIQ,iiq,json,tif,jpg,flight_data_mounted}' --changed-before 10s --type f --exec echo {} | \
         # remove far left path (/mnt/data/) dir for rsync
         cut -d'/' -f4- | \
         # cap max argument length for rsync
@@ -53,7 +53,7 @@ do
     # Find all files modified before N, excluding IIQ images, that are not append to.
     # This includes things like tif, jpg, json.
     # Give a little time, so the detector can run on images on the local drive
-    fdfind . $source_dir --exclude '*.{IIQ,iiq,txt,csv,dat}' --changed-before 10s --type f --exec echo {} | \
+    fdfind . $source_dir --exclude '*.{IIQ,iiq,txt,csv,dat,flight_data_mounted}' --changed-before 10s --type f --exec echo {} | \
         # remove far left dir for rsync
         cut -d'/' -f4- | \
         # cap max argument length for rsync
@@ -66,7 +66,7 @@ do
     echo "Copying & removing text files older than 1 day in $source_dir"
     ## Find all files that are appended to, which includes csvs, text files, and the .dat files
     ## The one day wait ensures that file is finished being written to.
-    fdfind . $source_dir --exclude '*.{IIQ,iiq,json,tif,jpg}' --changed-before 1day --type f --exec echo {} | \
+    fdfind . $source_dir --exclude '*.{IIQ,iiq,json,tif,jpg,flight_data_mounted}' --changed-before 1day --type f --exec echo {} | \
         # remove far left dir for rsync
         cut -d'/' -f4- | \
         # cap max argument length for rsync
