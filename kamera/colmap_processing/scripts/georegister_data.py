57c07649e913cc406d503704f45a3776f54b15cf
romleiaj <adam.romlein@gmail.com>
2026-06-17 11:34:55 -0400

    Remove all old license strings, replace GUI license with Apache 2.0.

--- kamera/colmap_processing/scripts/georegister_data.py
+++ kamera/colmap_processing/scripts/georegister_data.py
@@ -1,36 +1,5 @@
 #!/usr/bin/env python
 """
-ckwg +31
-Copyright 2019 by Kitware, Inc.
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
- * Redistributions of source code must retain the above copyright notice,
-   this list of conditions and the following disclaimer.
-
- * Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
- * Neither name of Kitware, Inc. nor the names of any contributors may be used
-   to endorse or promote products derived from this software without specific
-   prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-==============================================================================
-
 This script helps to georegister the 3-D reconstruction coordinate system used
 by Colmap. We assume that enough of the scene is jointly visible (e.g., enough
 high-altitude views) so that georegistration is a rigid rotation and isotropic
@@ -41,8 +10,6 @@ first saving images with keypoints associated with a sparse-reconstructed 3-D
 point superimposed. These images should be loaded into the
 landmark_registration GUI. The ENU origin should be added as a point with
 latitude and longitude both zero.
-
-
 """
 from __future__ import division, print_function, absolute_import
 import numpy as np
--- src/kitware-ros-pkg/wxpython_gui/src/wxpython_gui/cfg.py
+++ src/kitware-ros-pkg/wxpython_gui/src/wxpython_gui/cfg.py
@@ -234,36 +234,11 @@ geod = pygeodesy.geoids.GeoidPGM(geod_filename)
 PAT_BRACED = re.compile(r"\{(\w+)\}")
 
 
-LICENSE_STR = "".join(
-    [
-        "Copyright 2018 by Kitware, Inc.\n",
-        "All rights reserved.\n\n",
-        "Redistribution and use in source and binary forms, with or without ",
-        "modification, are permitted provided that the following conditions are met:",
-        "\n\n",
-        "* Redistributions of source code must retain the above copyright notice, ",
-        "this list of conditions and the following disclaimer.",
-        "\n\n",
-        "* Redistributions in binary form must reproduce the above copyright notice, ",
-        "this list of conditions and the following disclaimer in the documentation ",
-        "and/or other materials provided with the distribution.",
-        "\n\n",
-        "* Neither name of Kitware, Inc. nor the names of any contributors may be ",
-        "used to endorse or promote products derived from this software without ",
-        "specific prior written permission.",
-        "\n\n",
-        "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ",
-        "'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED ",
-        "TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR ",
-        "PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE ",
-        "LIABLE FORANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR ",
-        "CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF ",
-        "SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS ",
-        "INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN ",
-        "CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ",
-        "ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE ",
-        "POSSIBILITY OF SUCH DAMAGE.",
-    ]
+LICENSE_STR = (
+    "Licensed under the Apache License, Version 2.0 (the \"License\"); "
+    "you may not use this file except in compliance with the License. "
+    "You may obtain a copy of the License at "
+    "http://www.apache.org/licenses/LICENSE-2.0"
 )
 
 # =================== END GLOBALS ===============================
